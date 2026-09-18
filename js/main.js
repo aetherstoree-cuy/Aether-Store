a87786e4754233ef16156bc2c8936b9fb3e7981f
Aether <aetherstoree@gmail.com>
2026-09-18 13:07:43 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ mybutton = document.getElementById('myBtn');
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
 mybutton.style.display = 'block';
 } else {
 mybutton.style.display = 'none';
